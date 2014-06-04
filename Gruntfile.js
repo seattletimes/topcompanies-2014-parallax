39e50c34f4ce0de74cc38cc4626a2dfe49d29ae0
Thomas Wilburn <twilburn@seattletimes.com>
2014-06-03 17:13:27 -0700

    First

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  var csv = require("csv");
+  var fs = require("fs");
+
+  grunt.initConfig({
+    watch: {
+      css: {
+        files: ["*.less"],
+        tasks: ["less", "build"],
+        options: {
+          spawn: true,
+
+        }
+      },
+      rest: {
+        files: ["*.js", "template.html"],
+        tasks: ["build"],
+        options: {
+          spawn: true
+        }
+      }
+    },
+    less: {
+      def: {
+        src: "style.less",
+        dest: "style.css"
+      }
+    }
+  });
+
+  grunt.registerTask("default", ["less", "build", "watch"]);
+
+  grunt.registerTask("build", function() {
+    var template = fs.readFileSync("template.html", { encoding: "utf8" });
+    var script = fs.readFileSync("script.js", { encoding: "utf8" });
+    var style = fs.readFileSync("style.css", { encoding: "utf8" });
+
+    var coCSV = fs.readFileSync("companies.csv", { encoding: "utf8" });
+    var parser = csv.parse({
+      columns: true,
+      auto_parse: true
+    });
+    var rows = [];
+    parser.on("data", function(row) {
+      rows.push(row);
+    });
+    parser.on("finish", function(all) {
+
+
+    var output = grunt.template.process(template, {
+      data: {
+        js: script,
+        css: style,
+        companies: rows
+      }
+    });
+    fs.writeFileSync("index.html", output);
+    });
+    parser.write(coCSV);
+    parser.end();
+  });
+
+}
---
+++ script.js
@@ -0,0 +1,84 @@
+var canvas = document.querySelector("canvas.backdrop");
+var context = canvas.getContext("2d");
+
+var qsa = function(s) { return Array.prototype.slice.call(document.querySelectorAll(s)) };
+
+var texts = qsa(".scroll-text");
+var sections = qsa(".scroll-section");
+var images = {};
+var loaded = 0;
+var checkLoaded = function() {
+  loaded++;
+  if (loaded == sections.length) {
+    redraw();
+  }
+};
+sections.forEach(function(section) {
+  var url = section.getAttribute("data-image");
+  var image = new Image();
+  image.onload = checkLoaded;
+  image.src = url;
+  images[url] = image;
+});
+
+var redraw = function(e) {
+  var top = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = window.innerHeight;
+  var width = window.innerWidth;
+  var bottom = top + height;
+  canvas.width = width;
+  canvas.height = height;
+  sections.forEach(function(section, i) {
+    var bounds = section.getBoundingClientRect();
+    if (bounds.bottom < 0) return;
+    if (bounds.top > height) return;
+    var url = section.getAttribute("data-image");
+    var image = images[url];
+    var imgWidth = width;
+    var imgHeight = imgWidth / image.width * image.height;
+    var imgX = 0;
+    if (imgHeight < height) {
+      imgHeight = height;
+      imgWidth = imgHeight / image.height * image.width;
+      imgX = (width - imgWidth) / 2;
+    }
+
+    var imgTop = bounds.top;
+    if (imgTop <= 0) {
+      imgTop = 0;
+      if (bounds.bottom < height) {
+        imgTop = bounds.bottom - height;
+      }
+    }
+
+    //create parallax
+    var offset = 0;
+    if (bounds.top < 0) {
+      //how much bigger is the box than the screen?
+      var buffer = bounds.height - height;
+      //how much bigger is the image than the screen?
+      var overscan = imgHeight - height;
+      //how far into the image are we?
+      var progress = bounds.top / buffer;
+      //clamp progress at 1
+      if (progress < -1) {
+        progress = -1;
+      }
+      //figure out offset from progress through the overscan
+      offset = progress * overscan;
+    }
+
+    //optimize: if it's only a chunk of the image, why draw the whole thing?
+    //figure out sourceRect, destRect, use those
+    context.drawImage(image, imgX, imgTop + offset, imgWidth, imgHeight);
+  });
+  texts.forEach(function(text) {
+    var textBounds = text.getBoundingClientRect();
+    if (textBounds.top > height) return;
+    if (textBounds.bottom < 0) return;
+    text.style.opacity = 1 - ((textBounds.bottom - height) / height);
+  });
+}
+
+window.addEventListener("scroll", redraw);
+window.addEventListener("resize", redraw);
